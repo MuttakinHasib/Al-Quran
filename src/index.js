b1bd232a9b7adbcc84f41fd3dbbe255ef1f83776
MuttakinHasib <hasibmolla28@gmail.com>
2020-12-27 00:22:02 +0600

    initial commit :comet:

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import enTranslations from '@shopify/polaris/locales/en.json';
-import { AppProvider, Page, Card, Button } from '@shopify/polaris';
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider i18n={enTranslations}>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
